2b7cb7615fdd801cfd63db8a3b2532569a072e23
Vahan9797 <vahan9797@gmail.com>
2018-03-18 22:59:45 +0400

    ADD new features for env func; moved scss mixins and vars to index.scss

--- backend-implementation/config/environment.js
+++ backend-implementation/config/environment.js
@@ -3,12 +3,21 @@ const fileExists = require('fs').existsSync;
 const path = require('path');
 
 // for every build reload and every file .env will be loaded, but only once;
-module.exports = function env(key) {
+module.exports = function env(/* arguments */) {
+	const args = arguments;
 	if(!env.LOAD_ENV_ONCE) {
 		fileExists(path.join(__dirname, '../../override.env')) ? load(path.join(__dirname, '../../override.env')) : load();
 		Object.defineProperty(env, 'LOAD_ENV_ONCE', { value: true });
 		console.log('.env variables loaded');
 	}
 
-	return process.env[key];
+    switch(args.length) {
+		case 1:
+			if(Array.isArray(args[0])) return [...args[0]].map(arg => process.env[arg]);
+            return process.env[args[0]];
+		case 2:
+			return process.env[args[0]] !== args[1];
+		default:
+			return process.env;
+	}
 }
--- backend-implementation/helpers/Storage.js
+++ backend-implementation/helpers/Storage.js
@@ -1,37 +1,37 @@
-import { RESPONSE, MAX_FILE_SIZE, MESSAGES } from "./constants";
+import { RESPONSE, MAX_FILE_SIZE, IMG_FILES_DIR, MESSAGES } from "./constants";
 import fs from 'fs-extra';
 
 const { FORBIDDEN, ERROR } = RESPONSE;
 
 export default class Storage {
 	static upload(form) {
-    const req = typeof form.req === 'object' && form.req;
-    delete form.req;
+        const req = typeof form.req === 'object' && form.req;
+        delete form.req;
 
-    return new Promise((resolve, reject) => {
-    	form.parse(req, (err, fields, { fileUploaded }) => {
-        const { size, msg } = MAX_FILE_SIZE;
+        return new Promise((resolve, reject) => {
+            form.parse(req, (err, fields, { fileUploaded }) => {
+                const { size, msg } = MAX_FILE_SIZE;
 
-        if(err) {
-      		err.status = err.status || ERROR;
-          reject(err);
-        }
-        if(fileUploaded.size > size) {
-          const err = new Error(msg);
-          err.status = FORBIDDEN;
-          reject(err);
-        }
+                if(err) {
+                    err.status = err.status || ERROR;
+                  reject(err);
+                }
+                if(fileUploaded.size > size) {
+                  const err = new Error(msg);
+                  err.status = FORBIDDEN;
+                  reject(err);
+                }
 
-        fs.rename(fileUploaded.path, `${IMG_FILES_DIR}/${req.body.file.name}`, err => {
-          if(err) {
-          		err.status = err.status || ERROR;
-              reject(err);
-          }
-          console.log('File uploaded and renamed');
-          resolve();
-        });
-    	})
-    })
+                fs.rename(fileUploaded.path, `${IMG_FILES_DIR}/${req.body.file.name}`, err => {
+                  if(err) {
+                        err.status = err.status || ERROR;
+                      reject(err);
+                  }
+                  console.log('File uploaded and renamed');
+                  resolve();
+                });
+            })
+        })
 	}
 
 	static multiUpload(form) {
